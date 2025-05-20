1c236bb36515487fc1937573c693e6c4f00ed465
suinmi24 <yjadeone@gmail.com>
2025-05-20 10:56:46 +0900

    domain/Administratorの作成

---
+++ src/main/java/com/example/domain/Administrator.java
@@ -0,0 +1,63 @@
+package com.example.domain;
+
+/*
+* 管理者情報を表すドメイン.
+* ID,名前,メールアドレス,パスワードの管理者情報を表す。
+ */
+
+public class Administrator {
+
+    /** ID */
+    private Integer id;
+
+    /** 名前 */
+    private String name;
+
+    /** メールアドレス */
+    private String mailAddress;
+
+    /** パスワード */
+    private String password;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrator{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", mailAddress='" + mailAddress + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
